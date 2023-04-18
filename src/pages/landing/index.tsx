620ff72f0b5b1dd374e26f854d950254f0756b60
samanthareyes <samntha.reyes@gmail.com>
2023-04-17 17:54:04 -0700

    Added Landing Page IMG and .tsx file
    Added Landing page index.tsx and landing page img to public folder

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+    return ( 
+        <div className="h-full">
+            <img src="url('../public/landing.jpg)"></img>
+        </div>        
+    );
+}
+ 
+export default Landing;

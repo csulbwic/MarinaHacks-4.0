f1df8936e1fb903a2963e8e44befa27774309d4e
Rodolfo Sandoval <rodolfo.rudy.sandoval@gmail.com>
2023-04-21 22:36:01 -0700

    info section complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
-import { Contact, Faq, PastWinners, Pillars, Sponsors } from "@/views"
+import { Contact, Faq, Info, PastWinners, Pillars, Sponsors } from "@/views"
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen bg-gradient-to-b from-mhblue via-mhpink-200 to-mhpurple-100">
+      <Info />
       <Pillars />
       <PastWinners />
       <Sponsors />
       <Faq />
-      <Contact />
+      {/* <Contact /> */}
     </div>
   )
 }

4cfa24a3304059b93f4af61de6ea41e6b68c274b
samanthareyes <samntha.reyes@gmail.com>
2023-04-21 22:01:57 -0700

    changes to Contact
    Created flex. with two sub divs

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -4,20 +4,21 @@ import Link from 'next/link';
 import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa"
 export const Contact = () => {
   return (
-      <div className="h-screen w-full bg-gradient-to-br from-[#459CB8] via-[#7275B6] to-[#7E76B1] flex items-start">
-        <div className='absolute'></div>
+      <div className="h-screen w-full flex justify-center align-middle">
+        <div >
           <Image
             src={"/images/logo_turtle.png"}
             alt='wiclogo'
             width="588"
             height="115"
           />
-        <div className="absolute top-[20%] right-[5%]">
-          <h1 className="text-6xl leading-[77px] text-white opacity-50"> Connect With Us </h1>
+        </div>
+        <div className="flex-col">
+          <h1 className="text-6xl leading-[77px] text-white opacity-50"> Connect With Us </h1><br></br>
 
-          <h2 className="text-base leading-[19px] text-white opacity-50">For general questions or sponsorship inquiries, please email us at csulbwic@gmail.com</h2>
+          <h2 className="text-base leading-[19px] text-white opacity-50">For general questions or sponsorship inquiries, please email us at csulbwic@gmail.com</h2><br></br>
 
-          <div className="flex column">
+          <div className="flex">
             < a href="https://csulbwic.com/" target='_blank'>
               <Image
                 src={"/images/wic_logo_white.png"}
@@ -27,16 +28,16 @@ export const Contact = () => {
               />
             </a>
             <a href="https://www.facebook.com/wic.csulb/" target='_blank'>
-              <FaFacebook className="h-[50px] w-[60px] color-white" />
+              <FaFacebook className="h-[50px] w-[60px] text-white" />
             </a>
             <a href="https://www.instagram.com/csulbwic/?hl=en" target='_blank'>
-              <button><FaInstagram className="h-[50px] w-[60px] color: white" /></button>
+              <button><FaInstagram className="h-[50px] w-[60px]  text-white" /></button>
             </a>
             <a href="https://www.linkedin.com/company/csulbwic/" target='_blank'>
-              <button><FaLinkedin className="h-[50px] w-[60px] color: white"/></button>
+              <button><FaLinkedin className="h-[50px] w-[60px]  text-white"/></button>
             </a>
             <a href="https://twitter.com/csulbwic" target='_blank'>
-              <button><FaTwitter className="h-[50px] w-[60px] color: white" /></button>
+              <button><FaTwitter className="h-[50px] w-[60px]  text-white" /></button>
             </a>
           </div>
         </div>

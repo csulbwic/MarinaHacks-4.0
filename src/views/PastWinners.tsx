9ac65f9cc373ca2fac77a1dcb229995bcaece112
samanthareyes <samntha.reyes@gmail.com>
2023-04-21 23:52:09 -0700

    working on PastWinners
    Added a star to represent past winner

--- src/views/PastWinners.tsx
+++ src/views/PastWinners.tsx
@@ -1,9 +1,22 @@
 export const PastWinners = () => {
   return(
-    <div className="flex justify-center items-center">
-      <h1 className="text-6xl font-bold text-black opacity-50">
-        Past Projects
-      </h1>
+    <div className="flex flex-col justify-center items-center pt-16 gap-16">
+      <div className="flex justify-center items-center">
+        <h1 className="text-6xl font-bold text-black opacity-50">
+          Past Winners
+        </h1>
+        </div>
+
+      <div className='flex-row items-center'>
+        <div className=" mask mask-star-2 flex-col justify-center items-center p-12 rounded-xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
+          <h1 className="text-2xl text-white place-self-stretch">
+            2022
+          </h1>
+          <h1 className="text-2xl text-white">
+            Winners
+          </h1>
+        </div>
+      </div>
     </div>
   )
 }

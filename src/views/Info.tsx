978f07cbc6cb2542b5a62f9bd012f761190f2f5e
msjulietruong <msjulietruong@gmail.com>
2024-03-31 18:48:17 -0700

    changed width and height of images + placements, also made a shark speech bubble appear that says "Go Beach!" when clicked

--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -1,69 +1,89 @@
 import Image from 'next/image'
 import style from "@/styles/landing.module.css";
+import React, { useState } from 'react';
+
 export const Info = () => {
-  return (
-      <div id="home" className="flex flex-col justify-center items-center">
+    const [showChatBubble, setShowChatBubble] = useState(false);
+
+    const toggleChatBubble = () => {
+        setShowChatBubble(!showChatBubble);
+    };
+    return (
+
+    <div id="home" className="flex flex-col justify-center items-center">
         <div className="flex flex-col justify-center items-center">
-          <Image
-              src={"/images/web-anim/landing-shark.png"}
-              alt='background'
-              width="7560"
-              height="4904"
-              className="object-contain"
-          />
-          <Image
-              alt = 'wic logo'
-              src={"/images/web-anim/WiC-logo-black.png"}
-              width="110"
-              height="102"
-              className={style.logo}
-          />
-          <Image
-              alt = 'MarinaHacks 3.5 Logo'
-              src={"/images/web-anim/MH-3.5-logo.png"}
-              width="730"
-              height="530"
-              className={style.logoo}
-          />
-          <h1 className="text-6xl font-bold text-mhpurple-400">
-            April 21st - 22rd, 2023
-          </h1>
+            <div className="flex absolute right-1.5 top-100 h-2553 w-1631">
+                <a href="#" onClick={toggleChatBubble} style={{position: 'relative', top: '28px', opacity: 0}}>
+                    <Image src="/images/web-anim/simplified.png" alt="Clickable shark" width="1631"
+                           height="2553" className=" responsive h-full w-full object-cover"/>
+                </a>
+            </div>
+            <Image
+                src={"/images/web-anim/landing-shark.png"}
+                alt='background'
+                width="7560"
+                height="4904"
+                className="object-contain"
+            />
+            <Image
+                alt='wic logo'
+                src={"/images/web-anim/WiC-logo-black.png"}
+                width="125"
+                height="115"
+                className={style.logo}
+            />
+            <Image
+                alt='MarinaHacks 3.5 Logo'
+                src={"/images/web-anim/MH-3.5-logo.png"}
+                width="685"
+                height="500"
+                className={style.logoo}
+            />
+            <h1 className="text-6xl font-bold text-mhpurple-400">
+                April 21st - 22rd, 2023
+            </h1>
         </div>
         <div className="flex justify-center items-center gap-24">
-          <div
-              className=" flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
-            <h1 className="text-2xl pb-6">
-              Attend MarinaHacks 3.0
-            </h1>
-            <a href='https://docs.google.com/forms/d/e/1FAIpQLSfLktsjhdhQil4zA0cEiXslVB_OATcziWzM8d66v2YLpL1W8A/closedform'>
-              <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
-                Apply Now
-              </button>
+            <div
+                className=" flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
+                <h1 className="text-2xl pb-6">
+                    Attend MarinaHacks 3.0
+                </h1>
+                <a href='https://docs.google.com/forms/d/e/1FAIpQLSfLktsjhdhQil4zA0cEiXslVB_OATcziWzM8d66v2YLpL1W8A/closedform'>
+                <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
+                    Apply Now
+                </button>
             </a>
-          </div>
-          <div
-              className=" flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
-            <h1 className="text-2xl pb-6">
-              Volunteer at MarinaHacks 3.0
-            </h1>
-            <a href='https://docs.google.com/forms/d/e/1FAIpQLSf0nTpt80nF6AcJYCSSpwemOtVZlxumAgxxGqzYO0TFIm9C1A/closedform'>
-              <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
-                Apply Now
-              </button>
-            </a>
-          </div>
         </div>
-        <div className="text-mhpurple-400 font-bold flex flex-col gap-8 w-5/6">
-          <p className="text-2xl">
-            {"Women in Computing is hosting MarinaHacks: California State University.-Long Beach's annual women-centric hackathon- This free event is dedicated to bringing together women to hone their skills, develop confidence and fill the gender gap within the competitive tech industry-"}
-          </p>
-          <h1 className="text-5xl">
-            Marinahacks 3.0
-          </h1>
-          <p className="text-2xl">
-            {"This year's MarinaHacks will take place as fully in person 24-hour event from April 22-23rd in the CSULB USU Ballrooms. You will be able to win prizes, network with other students, attend workshops (i.e. Google, Cisco, etc.), in order to build your resume and most importantly have fun!"}
-          </p>
+        <div
+            className=" flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
+                    <h1 className="text-2xl pb-6">
+                        Volunteer at MarinaHacks 3.0
+                    </h1>
+                    <a href='https://docs.google.com/forms/d/e/1FAIpQLSf0nTpt80nF6AcJYCSSpwemOtVZlxumAgxxGqzYO0TFIm9C1A/closedform'>
+                        <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
+                            Apply Now
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div className="text-mhpurple-400 font-bold flex flex-col gap-8 w-5/6">
+                <p className="text-2xl">
+                    {"Women in Computing is hosting MarinaHacks: California State University.-Long Beach's annual women-centric hackathon- This free event is dedicated to bringing together women to hone their skills, develop confidence and fill the gender gap within the competitive tech industry-"}
+                </p>
+                <h1 className="text-5xl">
+                    Marinahacks 3.0
+                </h1>
+                <p className="text-2xl">
+                    {"This year's MarinaHacks will take place as fully in person 24-hour event from April 22-23rd in the CSULB USU Ballrooms. You will be able to win prizes, network with other students, attend workshops (i.e. Google, Cisco, etc.), in order to build your resume and most importantly have fun!"}
+                </p>
+                {showChatBubble && (
+                    <div className={"flex absolute top-20 left-150 p-4 bg-white rounded-lg shadow-md " + style.bubble} >
+                        <p className="text-gray-800">Go Beach!</p>
+                    </div>
+
+                )}
+            </div>
         </div>
-      </div>
-  )
+    )
 }

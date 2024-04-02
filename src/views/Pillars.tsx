9087186dec11831a90f754d0a4e3d7a2357547a6
zlaud <laudkimberly@gmail.com>
2024-04-01 23:03:19 -0700

    pillar tweaks

--- src/views/Pillars.tsx
+++ src/views/Pillars.tsx
@@ -13,7 +13,7 @@ export const Pillars = () => {
       <div className="container flex flex-col justify-center items-center gap-6 mb-10 relative z-10">
         <div className="w-full m-5 flex flex-row justify-start items-center rounded-2xl p-2 bg-[#A798FF]">
           <h1 className="text-5xl my-6 w-4/12 text-center">
-            Hackers' Favorites
+            {"Hackers' Favorites"}
           </h1>
           <h1 className="text-8xl my-6 w-1/12">: </h1>
           <p className="text-2xl w-5/12 text-bold text-center flex items-center justify-start">
@@ -22,7 +22,7 @@ export const Pillars = () => {
         </div>
         <div className="w-full m-5 flex flex-row-reverse justify-start items-center rounded-2xl p-2 bg-[#8496FE]">
           <h1 className="text-5xl my-6 w-4/12 text-center">
-            Best Social Goods
+            {"Best Social Goods"}
           </h1>
           <h1 className="text-8xl pl-6 my-6 w-1/12">{":"}</h1>
           <p className="text-2xl w-6/12 text-bold text-center">
@@ -31,7 +31,7 @@ export const Pillars = () => {
         </div>
         <div className="w-full m-5 flex flex-row justify-start items-center rounded-2xl p-2 bg-[#6085F2]">
           <h1 className="text-5xl my-6 w-4/12 text-center">
-            Best Entertainment
+            {"Best Entertainment"}
           </h1>
           <h1 className="text-8xl pl-6 my-6 w-1/12">{":"}</h1>
           <p className="text-2xl w-6/12 text-bold text-center">
@@ -42,3 +42,4 @@ export const Pillars = () => {
     </div>
   );
 };
+``;

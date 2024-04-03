e17007aa081b9a797e2e74397c200fa782d220b2
rokamune <reynaoaguirre@gmail.com>
2024-04-03 14:33:48 -0700

    color background gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,31 +9,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        mhblue: "#7CCDf3",
-        mhsky: "#9bb0da",
-        mhpink : {
-          100: "#E8ACC9",
-          200: "#E38FB6",
-          300: "#CF7DAD",
-        },
-        mhpurple: {
-          100: "#B47AAE",
-          200: "#9B78B0",
-          300: "#7E76B1",
-          400: "#7275B6",
-        }
+        hue1: "#EEFDFF",
+        hue2: "#DAF6FF",
+        hue3: "#C5EFFF",
+        hue4: "#9BE0FF",
+        hue5: "#71D1FF",
+        hue6: "#5CCAFF",
+        hue7: "#47C2FF"
       },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
       },
     },
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    }
   },
   plugins: [require("daisyui")],
 };

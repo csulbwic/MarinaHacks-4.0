804e13717a1ec62da53523152cf2f05fe126d469
rokamune <reynaoaguirre@gmail.com>
2024-04-03 16:02:12 -0700

    FAQ -Zophia

--- src/Data/questions.tsx
+++ src/Data/questions.tsx
@@ -1,26 +1,33 @@
 export const questions = [
   {
-    question: "When is MarinaHacks?",
-    answer: "MarinaHacks will take place from 11:00 AM Saturday, April 22nd, 2023 to 4:00 PM Sunday, April 23rd, 2023."
+    question: "When is MarinaHacks 3.5?",
+    answer:
+      "MarinaHacks will take place from 9:00 AM Saturday, April 13th, 2024 to 2:00 PM Sunday, April 14th, 2024.",
   },
   {
     question: "What takes place at MarinaHacks?",
-    answer: "There will be several workshops hosted by our sponsors, interactive socials, and a change to compete for prizes!"
+    answer:
+      "There will be several workshops hosted by our sponsors/clubs on campus, interactive socials, and a chance to compete for prizes!",
   },
   {
     question: "Will this event be in-person?",
-    answer: "MarinaHacks 3.0 will be in-person with an exception for some workshops. Maximum capacity 40 people In-person registration is first come, first serve for those that apply to attend MarinaHacks."
+    answer:
+      "MarinaHacks 3.5 will be all virtual! The hackathon will be hosted on Discord, and we’ll be using Devpost to submit the projects.",
   },
   {
-    question: "What are the benefits of sponsoring/hosting a workshop at MarinaHacks?",
-    answer: "Those that sponsor either with monetary donations or non-monetary such as workshops will receive company outreach & exposure to students at CSULB, be able to highlight your company's emerging products & technology, partake in mentorship opportunities, receive access to the member base's resumes, and most importantly, become an ally for a more inclusive & open tech industry.\nCheck out our contact section if you'd like to reach out to us for collaboration inquiries and receive our Sponsorship Package."
+    question:
+      "What are the benefits of sponsoring/hosting a workshop at MarinaHacks?",
+    answer:
+      "Those that sponsor either with monetary donations or non-monetary such as workshops will receive company outreach & exposure to students at CSULB, be able to highlight your company's emerging products & technology, partake in mentorship opportunities, receive access to the member base's resumes, and most importantly, become an ally for a more inclusive & open tech industry. Check out our contact section if you'd like to reach out to us for collaboration inquiries and receive our Sponsorship Package.",
   },
   {
     question: "What are the prizes for the hackathon winners?",
-    answer: "Hackathon winners per each pillar will receive the specified prizes in the MarinaHacks Devpost."
+    answer:
+      "Hackathon winners per each pillar will receive the specified prizes in the MarinaHacks Devpost.",
   },
   {
     question: "What are the benefits in competing in the hackathon?",
-    answer: "Hackathon winners per each pillar will receive the specified prizes in the MarinaHacks Devpost."
-  }
-]
+    answer:
+      "Hackathon winners per each pillar will receive the specified prizes in the MarinaHacks Devpost.",
+  },
+];

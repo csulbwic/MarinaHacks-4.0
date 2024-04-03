f3a525361b9956ef49ccf72c5f4cef429c0ef20f
rokamune <reynaoaguirre@gmail.com>
2024-04-03 15:21:52 -0700

    Pillars Layout

--- src/views/Pillars.tsx
+++ src/views/Pillars.tsx
@@ -1,43 +1,42 @@
-import { FaRegHandshake, FaRegHeart, FaTv } from "react-icons/fa"
+import { FaRegHandshake, FaRegHeart, FaTv } from "react-icons/fa";
 
 export const Pillars = () => {
   return (
-    <div id="pillars" className="flex flex-col justify-center items-center pt-12">
-      <h1 className="text-6xl font-bold text-black opacity-50 mt-20 mb-6">
-        Pillars
-      </h1>
-      <p className="text-2xl font-bold text-black opacity-50">
-        This year at MarinaHacks, develop an innovative project that targets one of our competition pillars.
-      </p>
-      <div className="flex flex-row justify-center items-center gap-6 my-10">
-        <div className="w-96 flex flex-col justify-center items-center rounded-2xl p-6 bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
-          <FaRegHandshake size='10rem' />
-          <h1 className="text-3xl my-6">
-            Connecting
+    <div
+      id="pillars"
+      className="flex flex-col justify-center items-center pt-12"
+    >
+      <h1 className="text-5xl font-bold text-[#57417A] mt-20 mb-2">Pillars</h1>
+      <div className="container flex flex-col justify-center items-center gap-6 mb-10 relative z-10">
+        <div className="w-full m-5 flex flex-row justify-start items-center rounded-2xl p-2 bg-[#A798FF]">
+          <h1 className="text-3xl font-bold my-6 w-4/12 text-center text-white">
+            {"Hacker's Favorites"}
           </h1>
-          <p className="text-1xl text-center">
-            {"Social distancing & virtual learning doesn't mean we should be disconnected. Make technology that can bring us together."}
+          <h1 className="text-8xl my-6 w-1/12">: </h1>
+          <p className="text-1xl w-5/12 text-bold text-center flex items-center justify-start text-white">
+            {"This category "}
           </p>
         </div>
-        <div className="w-96 flex flex-col justify-center items-center rounded-2xl p-6 bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
-          <FaRegHeart size='10rem' />
-          <h1 className="text-3xl my-6">
-            Well-being
+        <div className="w-full m-5 flex flex-row justify-start items-center rounded-2xl p-2 bg-[#8496FE]">
+          <h1 className="text-3xl font-bold my-6 w-4/12 text-center text-white">
+            {"Best Social Goods"}
           </h1>
-          <p className="text-1xl text-center">
-          {"COVID-19 has changed the lives of many in our communities. Create technology that can improve our own well-being, as well as the environment's."}
+          <h1 className="text-8xl my-6 w-1/12">: </h1>
+          <p className="text-1xl w-5/12 text-bold text-center flex items-center justify-start text-white">
+            {"Pillar description goes here"}
           </p>
         </div>
-        <div className="w-96 flex flex-col justify-center items-center rounded-2xl p-6 bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
-          <FaTv size='10rem' />
-          <h1 className="text-3xl my-6">
-            Connecting
+        <div className="w-full m-5 flex flex-row justify-start items-center rounded-2xl p-2 bg-[#6085F2]">
+          <h1 className="text-3xl font-bold my-6 w-4/12 text-center text-white">
+            {"Best Entertainment"}
           </h1>
-          <p className="text-1xl text-center">
-          Throughout the pandemic, people have been finding different ways to channel their creativity. Develop technology that brings your creativity to life.
+          <h1 className="text-8xl pl-6 my-6 w-1/12">{":"}</h1>
+          <p className="text-1xl w-5/12 text-bold text-center flex items-center justify-start text-white">
+            {"Pillar description goes here"}
           </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
+``;

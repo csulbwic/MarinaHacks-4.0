93916f6c16364cd7bfe115a086b4f270fd3cbe47
KinokoLu <myluwork004@gmail.com>
2024-04-04 02:19:42 -0700

    Sponsorship session

--- src/views/Sponsors.tsx
+++ src/views/Sponsors.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image'
+import style from '../styles/sponsor.module.css'
+ 
+
 
 export const Sponsors = () => {
   return (
@@ -6,15 +9,26 @@ export const Sponsors = () => {
       <h1 className="text-6xl font-bold text-black opacity-50 mt-20 mb-12">
         Sponsors
       </h1>
-      <div className="flex flex-row flex-wrap justify-center items-center gap-6 pb-6 ">
-        <div className="rounded-2xl p-12 bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
+      <div className={`${style.container}  flex flex-row flex-wrap justify-center items-center gap-6 pb-6`}>
+
+        <div className="rounded-2xl p-12 ">
           <Image
-            src={"/images/sponsors/coe.png"}
-            alt='background'
-            width="770"
-            height="100"
+            src = {"/images/sponsors/coe_logo.jpg"}
+            alt = 'background'
+            width={200}
+            height={200}
           />
         </div>
+
+        <div className="rounded-2xl p-12 ">
+          <Image
+            src = {"/images/sponsors/acm_logo1.png"}
+            alt = 'background'
+            width={200}
+            height={200}
+          />
+        </div>
+
         
       </div>
     </div>

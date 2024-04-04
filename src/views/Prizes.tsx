aa2262c58a89528acb8428d6a0e1e9bf3a2aa0a5
Eden <ecoding@pm.me>
2024-04-03 23:36:35 -0700

    Prizes v1
    As a lot of the website isn't done yet, I'm happy to iterate on this. I just wanted to focus on getting something in tonight.

--- src/views/Prizes.tsx
+++ src/views/Prizes.tsx
@@ -1,3 +1,4 @@
+
 import Link from 'next/link'
 import Image from 'next/image'
 
@@ -19,7 +20,15 @@ export const Prizes = () => {
             width="450"
             height="450"
           />
-    
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+            <p className="text-white text-2xl font-bold">Top notch headphones!</p>
+          </div>
+          <Image
+            src={"/images/prize_photos/headphones.png"}
+            alt="Headphones"
+            width="250"
+            height="250"
+          />
         </div>
 
         <div className="relative">
@@ -29,7 +38,17 @@ export const Prizes = () => {
             width="450"
             height="450"
           />
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+            <p className="text-white text-2xl font-bold">Peak performance mouse!</p>
+          </div>
+          <Image
+            src={"/images/prize_photos/mouse.png"}
+            alt="Mouse"
+            width="250"
+            height="250"
+          />
         </div>
+      
 
         <div className="relative">
           <Image
@@ -38,8 +57,16 @@ export const Prizes = () => {
             width="450"
             height="450"
           />
-
-        </div>
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+            <p className="text-white text-2xl font-bold">Hacker approved mousepad!</p>
+          </div>
+          <Image
+            src={"/images/prize_photos/mousepad.png"}
+            alt="Mousepad"
+            width="250"
+            height="250"
+          />
+          </div>
       </div>
     </div>
   )

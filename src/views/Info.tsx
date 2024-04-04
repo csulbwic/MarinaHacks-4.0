1e8358689b72dc06da6259a6cba7d28de0721075
ZachHunt <zachhunt437@gmail.com>
2024-04-03 17:24:13 -0700

    updated index.info
    google form and layout

--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -11,30 +11,54 @@ export const Info = () => {
           className="object-contain"
         />
         <h1 className="text-6xl font-bold text-mhpurple-400">
-          April 21st - 22rd, 2023
+          April 21st - 22rd, 2024
         </h1>
       </div>
       <div className="flex justify-center items-center gap-24">
-        <div className=" flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
+      <div className="flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-purple-400">
           <h1 className="text-2xl pb-6">
-            Attend MarinaHacks 3.0
+            Become a Participant
           </h1>
-          <a href='https://docs.google.com/forms/d/e/1FAIpQLSfLktsjhdhQil4zA0cEiXslVB_OATcziWzM8d66v2YLpL1W8A/closedform'>
+          <div className="h-4"></div>
+          <p className="text-white text-lg">Apply so that you can join a team and compete with other hackers.</p>
+          <div className="h-4"></div>
+          <div className="h-4"></div>
+          <a href='https://docs.google.com/forms/d/e/1FAIpQLSc4r0qtlzA3Kip3WVEgyN_RnBzBUq3TvY_GOkCQCCFPiEXJeQ/viewform?usp=sf_link'>
             <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
               Apply Now
             </button>
           </a>
         </div>
-        <div className=" flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
+        <div className="flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-purple-400">
+
           <h1 className="text-2xl pb-6">
-            Volunteer at MarinaHacks 3.0
+            Become a Judge
           </h1>
-          <a href='https://docs.google.com/forms/d/e/1FAIpQLSf0nTpt80nF6AcJYCSSpwemOtVZlxumAgxxGqzYO0TFIm9C1A/closedform'>
+          <div className="h-4"></div>
+          <p className="text-white text-lg">Apply to be a judge that will rate and rank the work of all hackers.</p>
+          <div className="h-4"></div>
+          <div className="h-4"></div>
+          <a href='https://docs.google.com/forms/d/e/1FAIpQLSc4r0qtlzA3Kip3WVEgyN_RnBzBUq3TvY_GOkCQCCFPiEXJeQ/viewform?usp=sf_link'>
             <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
               Apply Now
             </button>
           </a>
         </div>
+        <div className="flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-purple-400">
+
+          <h1 className="text-2xl pb-6">
+            Become a Volunteer
+          </h1>
+          <div className="h-4"></div>
+          <p className="text-white text-lg">Pick this if you want to be funny. Nobody wants to volunteer lol.</p>
+          <div className="h-4"></div>
+          <div className="h-4"></div>
+          <a href='https://docs.google.com/forms/d/e/1FAIpQLSc4r0qtlzA3Kip3WVEgyN_RnBzBUq3TvY_GOkCQCCFPiEXJeQ/viewform?usp=sf_link'>
+            <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
+              Apply Now
+            </button>
+          </a>
+        </div>  
       </div>
       <div className="text-mhpurple-400 font-bold flex flex-col gap-8 w-5/6">
         <p className="text-2xl">

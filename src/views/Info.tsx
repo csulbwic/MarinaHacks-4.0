1f63727edc3b27fbadb1df68c18809c6fed50ff1
ZachHunt <zachhunt437@gmail.com>
2024-04-04 16:35:57 -0700

    Update Info.tsx
    Updated links to google forms so that they accommodate judges and guest speakers

--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -24,7 +24,7 @@ export const Info = () => {
           <div className="h-4"></div>
           <div className="h-4"></div>
 
-          <a href='https://docs.google.com/forms/d/e/1FAIpQLSc4r0qtlzA3Kip3WVEgyN_RnBzBUq3TvY_GOkCQCCFPiEXJeQ/viewform?usp=sf_link'>
+          <a href='https://docs.google.com/forms/d/e/1FAIpQLSc4r0qtlzA3Kip3WVEgyN_RnBzBUq3TvY_GOkCQCCFPiEXJeQ/viewform?usp=sharing'>
             <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
               Apply Now
             </button>
@@ -40,12 +40,29 @@ export const Info = () => {
           <p className="text-white text-lg">Apply to be a judge that will rate and rank the work of all hackers.</p>
           <div className="h-4"></div>
           <div className="h-4"></div>
-          <a href='https://docs.google.com/forms/d/e/1FAIpQLSc4r0qtlzA3Kip3WVEgyN_RnBzBUq3TvY_GOkCQCCFPiEXJeQ/viewform?usp=sf_link'>
+          <a href='https://docs.google.com/forms/d/e/1FAIpQLSd-uMWQMbPGWr7lrueLpctAaYg23QsCSz92f9CtUcIVPZZqkg/viewform?usp=sharing'>
             <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
               Apply Now
             </button>
           </a>
         </div>
+
+        <div className="flex flex-col w-96 justify-center items-center p-6 rounded-2xl bg-purple-400">
+
+          <h1 className="text-2xl pb-6">
+            Become a Guest Speaker
+          </h1>
+          <div className="h-4"></div>
+          <p className="text-white text-lg">Apply if you want to provide insight as a guest speaker during the event.</p>
+          <div className="h-4"></div>
+          <div className="h-4"></div>
+          <a href='https://docs.google.com/forms/d/e/1FAIpQLSflLAa_6X-5Qjov5_qzQ71g7UObejnXUozxYXlbDsD_tKjhrw/viewform?usp=sharing'>
+            <button className="btn btn-primary rounded-full text-xl font-thin bg-purple-900/50">
+              Apply Now
+            </button>
+          </a>
+        </div>
+
       </div>
       <div className="text-mhpurple-400 font-bold flex flex-col gap-8 w-5/6">
         <p className="text-2xl">

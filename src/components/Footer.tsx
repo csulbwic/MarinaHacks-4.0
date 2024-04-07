48f65af4f34c1976045c18a757dbc29576eda8b7
Abhinav Bachu <abhibachu9@gmail.com>
2024-04-07 12:25:11 -0700

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,34 @@
-import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa"
-import Image from 'next/image'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import Image from 'next/image';
 
 export const Footer = () => {
   return (
-    <footer className="footer items-center p-4 bg-mhsky text-neutral-content">
+    <footer className="footer items-center p-4 text-neutral-content" style={{backgroundColor: '#7275B6'}}>
       <div className="items-center grid-flow-col">
-        < a href="https://csulbwic.com/" target='_blank'>
+        <a href="https://csulbwic.com/" target='_blank' rel="noopener noreferrer">
           <Image
             src={"/images/wic_logo_white.png"}
             alt='wiclogo'
-            width="100"
-            height="100"
+            width={100}
+            height={100}
           />
         </a>
         <p className="text-white">Copyright © MarinaHacks. All Rights Reserved</p>
       </div>
       <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a href="https://www.facebook.com/wic.csulb/" target='_blank'>
+        <a href="https://www.facebook.com/wic.csulb/" target='_blank' rel="noopener noreferrer">
           <FaFacebook className="h-8 w-8 text-white" />
         </a>
-        <a href="https://www.instagram.com/csulbwic/?hl=en" target='_blank'>
-          <button><FaInstagram className="h-8 w-8  text-white" /></button>
+        <a href="https://www.instagram.com/csulbwic/?hl=en" target='_blank' rel="noopener noreferrer">
+          <FaInstagram className="h-8 w-8 text-white" />
         </a>
-        <a href="https://www.linkedin.com/company/csulbwic/" target='_blank'>
-          <button><FaLinkedin className="h-8 w-8  text-white" /></button>
+        <a href="https://www.linkedin.com/company/csulbwic/" target='_blank' rel="noopener noreferrer">
+          <FaLinkedin className="h-8 w-8 text-white" />
         </a>
-        <a href="https://twitter.com/csulbwic" target='_blank'>
-          <button><FaTwitter className="h-8 w-8  text-white" /></button>
+        <a href="https://twitter.com/csulbwic" target='_blank' rel="noopener noreferrer">
+          <FaTwitter className="h-8 w-8 text-white" />
         </a>
       </div>
     </footer>
-  )
-}
+  );
+};

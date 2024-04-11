84b4e7387f23de72825785b42794ea4da57ab7f1
Ajay Bhawariya <47675480+AJAYBHAWARIYA@users.noreply.github.com>
2024-04-11 11:16:22 -0700

    Responsive Application
    1. Added carousal for larger screen and a vertical format for small screen
    2. Modified transformation of Jellyfish
    
    Co-Authored-By: hithasunkara19 <113158029+hithasunkara19@users.noreply.github.com>

--- src/views/Application.tsx
+++ src/views/Application.tsx
@@ -3,24 +3,24 @@ import Image from 'next/image'
 export const Application = () => {
     return (
     
-                    <div id="application" className="flex flex-col justify-center items-center pt-20 gap-16">
+    <div id="application" className="flex flex-col justify-center items-center pt-20">
 
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-6xl font-bold pt-10" style={{ color: '#0F516F' }}>
+        <h1 className="text-6xl text-center font-extrabold text-[#0F516F] mt-20 mb-2">
             Apply for Hackathon
         </h1>
-        <h1 className="text-4xl font-bold pt-5" style={{ color: '#7275B6' }}>
+        <h1 className="text-4xl font-bold mb-5" style={{ color: '#7275B6' }}>
             April 20th - 21st, 2024
         </h1>
       </div>
 
-        <div className="flex justify-center items-center gap-20">
-        <div className="flex flex-col w-[450px] justify-center items-center p-10 rounded-2xl" style={{backgroundColor: '#C2BBFF'}}>
+        <div className="flex flex-col md:flex-row md:carousel md:carousel-end md:w-11/12 md:rounded-box justify-center items-center text-center gap-10 mt-5">
+        <div className="flex flex-col md:carousel-item w-96 h-60 justify-center items-center p-10 rounded-2xl" style={{backgroundColor: '#C2BBFF'}}>
             <h1 className="text-2xl pb-6 font-bold" style={{ color: '#0F516F' }}>
             Become a Participant
             </h1>
             <div className="h-4"></div>
-            <p className="text-white font-bold text-center">Apply so that you can join a team and compete with other hackers.</p>
+            <p className="text-white font-bold text-center pb-2">Apply so that you can join a team and compete with other hackers.</p>
             <div className="h-4"></div>
             <div className="h-4"></div>
 
@@ -31,13 +31,13 @@ export const Application = () => {
             </a>
         </div>
 
-        <div className="flex flex-col w-[450px] justify-center items-center p-10 rounded-2xl" style={{backgroundColor: '#C2BBFF'}}>
+        <div className="flex flex-col md:carousel-item w-96 h-60 justify-center items-center p-10 rounded-2xl" style={{backgroundColor: '#C2BBFF'}}>
 
             <h1 className="text-2xl pb-6 font-bold" style={{ color: '#0F516F' }}>
             Become a Judge
             </h1>
             <div className="h-4"></div>
-            <p className="text-white font-bold text-center">Apply to be a judge that will rate and rank the work of all hackers.</p>
+            <p className="text-white font-bold text-center pb-2">Apply to be a judge that will rate and rank the work of all hackers.</p>
             <div className="h-4"></div>
             <div className="h-4"></div>
             <a href='https://docs.google.com/forms/d/e/1FAIpQLSd-uMWQMbPGWr7lrueLpctAaYg23QsCSz92f9CtUcIVPZZqkg/viewform'>
@@ -47,13 +47,13 @@ export const Application = () => {
             </a>
         </div>
 
-        <div className="flex flex-col w-[450px] justify-center items-center p-10 rounded-2xl" style={{backgroundColor: '#C2BBFF'}}>
+        <div className="flex flex-col md:carousel-item w-96 h-60 justify-center items-center p-10 rounded-2xl" style={{backgroundColor: '#C2BBFF'}}>
 
             <h1 className="text-2xl pb-6 font-bold" style={{ color: '#0F516F' }}>
             Become a Guest Speaker
             </h1>
             <div className="h-4"></div>
-            <p className="text-white font-bold text-center">Apply if you want to provide insight as a guest speaker during the event.</p>
+            <p className="text-white font-bold text-center pb-2">Apply if you want to provide insight as a guest speaker during the event.</p>
             <div className="h-4"></div>
             <div className="h-4"></div>
             <a href='https://docs.google.com/forms/d/e/1FAIpQLSd-uMWQMbPGWr7lrueLpctAaYg23QsCSz92f9CtUcIVPZZqkg/viewform'>

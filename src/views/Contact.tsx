fea498fd94b7705e78f141d0646c9932ab758b07
rokamune <reynaoaguirre@gmail.com>
2024-09-26 17:13:03 -0700

    contact info

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,52 +1,40 @@
 import style from '../styles/contact.module.css'
 import Image from 'next/image'
-import Link from 'next/link';
-import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa"
+import { FaFacebook, FaInstagram, FaLinkedin, FaEnvelope } from "react-icons/fa"
+
 export const Contact = () => {
   return (
-    <div id="contact" className="flex flex-col justify-center items-center py-20">
-      <h1 className="text-6xl text-center font-bold text-black opacity-50 mt-20 mb-6">
-        Hosted by Women in Computing at CSULB
+    <div id="contact" className={`flex flex-col justify-center items-center py-20 ${style.contactContainer}`}>
+      <h1 className="text-6xl text-center font-bold text-[#F26EAC] mt-10 mb-6">
+        Women in Computing at CSULB
       </h1>
       <div className="flex flex-row">
-        <div className="m-12 rounded-2xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
+        <div className={`m-12 rounded-2xl bg-gradient-to-r from-mhpurple-200 to-mhpurple-400 ${style.sharkAnimation}`}>
+     
           <Image
-            src={"/images/MarinaHacks_3.5_Round_Logo.png"}
+            src={"/images/logos_4.0/MarinaHacks_4.0_Logo.png"}
             alt='wiclogo'
-            width="350"
+            width="400"
             height="100"
           />
         </div>
         <div className="flex flex-col justify-center ">
-          <h1 className="text-6xl font-bold leading-[77px] text-white"> Connect With Us </h1><br></br>
+          <h2 className="text-3xl font-bold leading-[60px] text-[#45494C] text-center mb-8"> Contact Information</h2>
 
-          <h2 className="text-base font-bold leading-[19px] text-[#0F516F]">for general questions or sponsorship inquiries, please email us at csulbwic@gmail.com !</h2><br></br>
-
-          <div className="flex">
-            { /*< a href="https://csulbwic.com/" target='_blank'>
-              <Image
-                src={"/images/wic_logo_white.png"}
-                alt='wiclogo'
-                width="100"
-                height="100"
-              /> 
-            </a> */}
-            <a href="https://www.facebook.com/wic.csulb/" target='_blank'>
-              <FaFacebook className="h-[50px] w-[80px] text-white" />
-            </a>
+          <div className="flex flex-row gap-4 justify-center">
+            
             <a href="https://www.instagram.com/csulbwic/?hl=en" target='_blank'>
-              <button><FaInstagram className="h-[50px] w-[80px]  text-white" /></button>
+              <button><FaInstagram className="h-[60px] w-[80px]  text-[#FDBBD9] hover:text-[#F26EAC]" /></button>
+            </a>
+            <a href="mailto:csulbwic@gmail.com" target='_blank'>
+              <button><FaEnvelope className="h-[60px] w-[80px]  text-[#FDBBD9] hover:text-[#F26EAC] " /></button>
             </a>
             <a href="https://www.linkedin.com/company/csulbwic/" target='_blank'>
-              <button><FaLinkedin className="h-[50px] w-[80px]  text-white" /></button>
-            </a>
-            <a href="https://twitter.com/csulbwic" target='_blank'>
-              <button><FaTwitter className="h-[50px] w-[80px]  text-white" /></button>
+              <button><FaLinkedin className="h-[60px] w-[80px]  text-[#FDBBD9] hover:text-[#F26EAC]" /></button>
             </a>
           </div>
         </div>
       </div>
     </div>
-
   )
 }

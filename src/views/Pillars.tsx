8979ab5f632a01effd4fc41acae54ac99f75c4e6
rokamune <reynaoaguirre@gmail.com>
2024-09-27 09:21:48 -0700

    pillars: fixed compile error
    - use npm run build to check for errors

--- src/views/Pillars.tsx
+++ src/views/Pillars.tsx
@@ -1,4 +1,4 @@
-import styles from "@/styles/pillars.module.css"
+import styles from "../styles/pillars.module.css"
 
 
 export const Pillars = () => {
@@ -15,7 +15,7 @@ export const Pillars = () => {
         
         <div className={`${styles.pillarsBox} flex-1 min-w-[250px] max-w-[350px] flex flex-col`}>
               <h2 className="text-3xl font-extrabold mb-10 text-center" style={{ color: '#46494C' }}>
-                Hacker's Favorite
+                Hacker&#39;s Favorite
               </h2>
               <p className="text-1xl font-bold text-center text-white flex-grow flex items-center justify-center">
                 {"build something breathtaking that everyone will love! make it smart, make it useful, and make it fun!"}
@@ -36,70 +36,13 @@ export const Pillars = () => {
                 Best Entertainment
               </h2>
               <p className="text-1xl font-bold text-center text-white flex-grow flex items-center justify-center">
-                {"create technology that enhances engagement! this category focuses on individuality of the developer's technology, caputring everyone's attention!"}
+                {"create technology that enhances engagement! this category focuses on individuality of the developers technology, caputring everyones attention!"}
               </p>
         </div>
 
       </div>
 
     </div>
-
-    {/*
-    <div id="pillars" className="flex flex-col justify-center items-center sm:hidden">
-      <h1 className="text-6xl font-extrabold text-[#46494C] my-20">Pillars</h1>
-      <label className="swap swap-flip justify-center items-center w-fit h-fit mb-5">
-        <input type="checkbox" />
-        <div className="swap-on">
-          <div className="card w-96 h-80 justify-center items-center bg-[#FDBBD9]">
-            <p className="text-2xl w-9/12 text-bold font-bold text-center flex items-center justify-start text-white">
-              {"build something breathtaking that everyone will love! make it smart, make it useful, and make it fun!"}
-            </p>
-          </div>
-        </div>
-
-        <div className="swap-off">
-          <div className="card w-96 h-80 justify-center items-center bg-[#FDBBD9]">
-            <h1 className="text-3xl font-bold my-6 w-9/12 text-center text-white">
-             {"Hacker's Favorites"}
-            </h1>
-          </div>
-        </div>
-      </label>
-      <label className="swap swap-flip w-fit h-fit mb-5">
-        <input type="checkbox" />
-        <div className="swap-on">
-          <div className="card w-96 h-80 justify-center items-center bg-[#EB9AC1]">
-            <p className="text-2xl w-9/12 text-bold font-bold text-center flex items-center justify-start text-white">
-             {"innovation can spark a wave of positive change. develop technology that can help improve our society or our environment!"}
-            </p>
-          </div>
-        </div>
-        <div className="swap-off">
-          <div className="card w-96 h-80 justify-center items-center bg-[#EB9AC1]">
-            <h1 className="text-3xl font-bold my-6 w-9/12 text-center text-white">
-             {"Best Social Goods"}
-           </h1>
-          </div>
-        </div>
-      </label>
-      <label className="swap swap-flip w-fit h-fit mb-5">
-        <input type="checkbox" />
-        <div className="swap-on">
-          <div className="card w-96 h-80 justify-center items-center bg-[#F26EAC]">
-            <p className="text-2xl w-9/12 text-bold font-bold text-center flex items-center justify-start text-white">
-             {"create technology that enhances engagement! this category focuses on individuality of the developer's technology!"}
-            </p>
-          </div>
-        </div>
-        <div className="swap-off">
-          <div className="card w-96 h-80 justify-center items-center bg-[#F26EAC]">
-            <h1 className="text-3xl font-bold my-6 w-9/12 text-center text-white">
-             {"Best Entertainment"}
-           </h1>
-          </div>
-        </div>
-      </label>
-    </div> */}
     </> 
   );
 };

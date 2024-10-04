d450ed611a59208afe9a7d3f76452b902bc835d8
rokamune <reynaoaguirre@gmail.com>
2024-10-04 09:12:51 -0700

    updated team photos

--- src/views/Team.tsx
+++ src/views/Team.tsx
@@ -7,12 +7,29 @@ export const Team = () => {
       <div id ="team" className={styles.teamContainer}>
 
          <h1 className={`${styles.teamHeading} text-5xl mb-20`}>MarinaHacks Team</h1>
+         <div>
+                <h2 className={`${styles.teamSubHeading} ${styles.marinaHacksCommittee}`}>MarinaHacks 4.0 Director </h2>
+                <div className={`{$styles.memberContainer} mb-20` }>
+                    {/* Map Members of group member only */}
+                    {members.filter((member) => member.group === 1).map((item, index) => {
+                    return (
+                        <Profile 
+                            name={item.name} 
+                            role={item.role} 
+                            img={item.img} 
+                            key={index}
+                            committeeType="marinaHacksCommittee"
+                             />
+                    )
+                    })}
+                </div>
+            </div>
 
             <div>
                 <h2 className={`${styles.teamSubHeading} ${styles.officersCommittee}`}>Women in Computing Officers</h2>
                 <div className={styles.memberContainer}>
                     {/* Map Members of group member only */}
-                    {members.filter((member) => member.group === 1).map((item, index) => {
+                    {members.filter((member) => member.group === 2).map((item, index) => {
                     return (
                         <Profile 
                             name={item.name} 
@@ -31,7 +48,7 @@ export const Team = () => {
             <h2 className={`${styles.teamSubHeading} ${styles.websiteCommittee}`}>Website Committee</h2>
                 <div className={styles.memberContainer}>
                     {/* Map Members of group member only */}
-                    {members.filter((member) => member.group === 2).map((item, index) => {
+                    {members.filter((member) => member.group === 3).map((item, index) => {
                     return (
                         <Profile 
                             name={item.name} 
@@ -51,7 +68,7 @@ export const Team = () => {
             <h2 className={`${styles.teamSubHeading} ${styles.designCommittee}`}>Design Team</h2>
                 <div className={styles.memberContainer}>
                     {/* Map Members of group member only */}
-                    {members.filter((member) => member.group === 3).map((item, index) => {
+                    {members.filter((member) => member.group === 4).map((item, index) => {
                     return (
                         <Profile 
                             name={item.name} 

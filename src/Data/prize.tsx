8a22df7e84c3814f133a0d13a81d168d6e76f20a
rokamune <reynaoaguirre@gmail.com>
2024-10-24 11:05:07 -0700

    Revert "Merge pull request #31 from csulbwic/kinoko-priceSection-101924"
    This reverts commit 17b6b53fed0091afd5f08dbfe2c8784f615d361c, reversing
    changes made to 5f37ddbd4b469fe5beb37779600a8c838721f3e2.

--- src/Data/prize.tsx
+++ src/Data/prize.tsx
@@ -4,24 +4,24 @@
 
 export const Prize = [
     {
-        name : "BEST UI//UX  | kodak Printomatic Instant Camera",
+        name : "kodak Printomatic Instant Camera",
         PrizeImg : "/images/prizeImg/Camera.png",
         
     },
     {
-        name : "BEST HACKER | SONY WH-1000XM5 Wireless Noise",
+        name : "SONY WH-1000XM5 Wireless Noise",
         PrizeImg : "/images/prizeImg/Headphone.png"
     },
     {
-        name : "BEST SOCIAL GOOD | Owala Bottle + Sticker",
-        PrizeImg : "/images/prizeImg/Owala.png"
+        name : "Logitech Mouse MX Master 3s",
+        PrizeImg : "/images/prizeImg/LogitechMouse.png"
     },
     {
-        name : "BEST ENTERTAINMENT | Amazon Echo Dot",
-        PrizeImg : "/images/prizeImg/AmazonEchoDot.png"
+        name : "Nintendo Switch Life",
+        PrizeImg : "/images/prizeImg/Nintendo.png"
     },
     {
-        name : "BEST TECHNICAL | Sceptre E205W 20 Monitor",
+        name : "Sceptre E205W 20 Monitor",
         PrizeImg : "/images/prizeImg/SpectreMonitor.png"
     }
 ]

999613c498cd074fd5228741db9c6ea7ceda563c
rokamune <reynaoaguirre@gmail.com>
2024-10-24 11:04:52 -0700

    Revert "openAI chatbot-- updated .gitignore to exclude .env"
    This reverts commit 5f37ddbd4b469fe5beb37779600a8c838721f3e2.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
 import { Landing } from "@/components"
 import { Contact, Faq, Info, Application, Prizes, Pillars, Sponsors, Team} from "@/views"
 import Bubbles from "@/components/Bubbles/Bubbles"
-import IdeaGen from "@/components/IdeaGen"
 
 export default function Home() {
   return (
     <div>
       <Bubbles />
       <Landing />
-      <IdeaGen />
       <Info />
       <Application />
       <Team />
       <Pillars />
       <Prizes/>
       <Faq />
-
       <Contact />
     </div>
   )
